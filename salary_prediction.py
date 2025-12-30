8f12a61d1412837132699601638cdf5d33f69d34
Anima Shrestha <anima.shrestha@rts.com.np>
2025-12-30 12:28:13 +0000

    Added aggegated parameters to view

--- salary_prediction.py
+++ salary_prediction.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 import requests
 import json
+import pandas as pd
+from pandas import json_normalize
 
 token = str(input("Please enter the Token here"))
 
@@ -16,7 +18,7 @@ params = {
     "date_to" : str(input("Enter the end date in the format 2025-12-26T00:00:00"))
 }
 
-url = "https://aws.wscada.net/api/observation"
+url = "https://alpha.wscada.net/api/observation"
 
 response = requests.get(url, headers=headers, params=params)
 response.raise_for_status()
@@ -26,20 +28,55 @@ response.raise_for_status()
 
 data = response.json()
 
-with open("Data_test1.json", "a", encoding = "utf-8") as file:
+df = pd.DataFrame(data)
+print(df.columns)
+
+#df['datetime'] = pd.to_datetime(df['datetime'])
+
+#df.set_index('datetime', inplace=True)
+
+#aggregated_data = df.resample('H').mean()
+
+'''
+with open("Aggregated_Data.json", "a", encoding = "utf-8") as file:
     entry = {
         "timestamp" : datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         #"name" : name,
         #"mood" : mood
-        "data" : data
+        #"data" : aggregated_data.to_dict(orient='index')
+        #"data" : data
     }
-
     file.write(json.dumps(entry, indent=1) + "\n")
     #file.write(json.dumps(data, indent=1))
 
 #print(f"\n {name}, you feeling {mood}")
+'''
 print(json.dumps(data, indent=1))
+#print(json.dumps(aggregated_data.to_dict(orient='index', indent=1)))
 
+
+if 'data' in data:
+    data_items = json_normalize(data['data'])
+    print(data_items.head())
+else:
+    print("No 'data' key found in the response")
+
+
+if 'datetime' in data_items.columns and 'value' in data_items.columns:
+    data_items['datetime'] = pd.to_datetime(data_items['datetime'])
+
+    data_items.set_index('datetime', inplace=True)
+
+    data_resampled = data_items.resample('10min').mean()
+
+    aggregated_data = data_resampled.resample('h').mean()
+
+    aggregated_data = aggregated_data.shift(1, freq='h')
+
+    aggregated_data.index =  aggregated_data.index.strftime('%Y-%m-%d %H:%M:%S')
+    print(json.dumps(aggregated_data.to_dict(orient='index'),indent=1))
+else:
+    print("required columns {datetime and value} are not found")
 
 
 
