dc15ecb88f7c4931765278f7b6e5ca22c28fd1a9
Anima Shrestha <anima.shrestha@rts.com.np>
2026-01-05 09:32:33 +0000

    Aggregation and validation_1

--- aggregation_test1.py
+++ aggregation_test1.py
@@ -28,68 +28,57 @@ url = "https://alpha.wscada.net/api/analysis/more"
 response = requests.get(url, headers=headers, params=params)
 response.raise_for_status()
 
-#name = input("Hey, what is your name?").strip()
-#mood = input(f"\n tell me {name} how are you feeling?").strip()
-
 data = response.json()
-#tp print the index of the data
+#print the index of the data
 df = pd.DataFrame(data)
 #print(df.columns)
 
 # to print the response of the API call in json format
 #print(json.dumps(data, indent=1))
 
-all_values = []
+
 for station in data:
-    for parameter in station["parameters"]:
-        for entry in parameter["data"]:
-            all_values.append(entry["value"])        
-
-for value in all_values:
-    print(round(value, 2))
-
-
-
-#values = [entry["value"] for entry in data_dict["data"]]
-#print(round(values, 2))
-'''
-#print(json.dumps(aggregated_data.to_dict(orient='index', indent=1)))
-
-#to check whether the 'data' contains the key we need, extracting and normalizing it
-if 'data' in data:
-    data_items = json_normalize(data['data']) #flatten the 'data' field if its nested and check the structure
-    print(data_items.head())
-else:
-    print("No 'data' key found in the response")
-
-#check for 'datetime and value' columns
-
-if 'datetime' in data_items.columns and 'value' in data_items.columns:
-    #Convert datetime to pandas datetime object
-    data_items['datetime'] = pd.to_datetime(data_items['datetime'])
+    for parameters in station['parameters']:
+        data_items = json_normalize(parameters['data']) #flatten the 'data' field if its nested and check the structure
+            #print(data_items.head())
+        if 'time' in data_items.columns and 'value' in data_items.columns:
+            #Convert datetime to pandas datetime object
+            data_items['time'] = pd.to_datetime(data_items['time'])
     #setting datetime as index
-    data_items.set_index('datetime', inplace=True)
+            data_items.set_index('time', inplace=True)
     #resampling the data every 10 minute
-    data_resampled = data_items.resample('10min').mean()
+            data_resampled = data_items.resample('10min').mean()
+            data_resampled = round(data_resampled,2)
     
     #grouping the hourly intervals and calculating the aveage for each hour
-    aggregated_data = data_resampled.resample('h').mean()
+            aggregated_data = data_resampled.resample('h').mean()
 
     #shifting the result to allign with the next hour
-    aggregated_data = aggregated_data.shift(1, freq='h')
+            aggregated_data = aggregated_data.shift(0, freq='h')
 
     #Convert the index(timestamps) to string format for json serialization
-    aggregated_data.index =  aggregated_data.index.strftime('%Y-%m-%d %H:%M:%S')
+            aggregated_data.index =  aggregated_data.index.strftime('%Y-%m-%d %H:%M:%S')
     #print the aggregated data
-    print(json.dumps(aggregated_data.to_dict(orient='index'),indent=1))
-else:
-    print("required columns {datetime and value} are not found")
-
-'''
-
+                #print(json.dumps(aggregated_data.to_dict(orient='index'),indent=1))
+        else: 
+            print("required columns {datetime and value} are not found")
+print(json.dumps(aggregated_data.to_dict(orient='index'),indent=1))
 
 
+ten_minutevalue = []
+for stationten in data:
+    for parameterten in stationten["parameters"]:
+        if parameterten["parameter_id"] == 726:
+            for entry_ten in parameterten["data"]:
+                ten_minutevalue.append(entry_ten["value"])
 
-
-
+for valueten in ten_minutevalue:
+       print(round(valueten,2))
+       #if valueten in aggregated_data['value']:
+       if (aggregated_data['value'].round(2) == round(valueten,2)).any():
+           print("aggregation is okay")
+       else:
+           print("aggregation not okay")
+        
+           
 
